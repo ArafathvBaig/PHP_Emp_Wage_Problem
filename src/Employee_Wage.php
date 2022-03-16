22e4d639a11b60c98a80fde642871c8ef685e552
“ArafathvBaig” <arafathbaig1997@gmail.com>
2022-03-16 16:02:53 +0530

    Added: Employee wage problem for attendance

--- src/Employee_Wage.php
+++ src/Employee_Wage.php
@@ -1,2 +1,27 @@
 <?php
 echo "Welcome to Employee Wage Computation Problem";
+
+/**
+ * Author -> Arafath Baig
+ * PHP Version -> 8.0.9
+ * Class to compute employee wage problem
+ */
+class Employee_Wage
+{
+    /**
+     * Function to Check Employee is Present or Absent
+     * Non-Parameterized function
+     * Using rand() to generate the random of 0 or 1
+     */
+    function attendance()
+    {
+        $empCheck = rand(0, 1);
+        if ($empCheck == 1) {
+            echo "\nEmployee is Present";
+        } else {
+            echo "\nEmployee is Absent";
+        }
+    }
+}
+$employeeWage = new Employee_Wage();
+$employeeWage->attendance();
