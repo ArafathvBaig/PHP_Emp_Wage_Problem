bfb4e3a8aa6a91fdb2320c1019386a3fec30a6b4
“ArafathvBaig” <arafathbaig1997@gmail.com>
2022-03-16 16:44:37 +0530

    Update: Using arrays to store company names and company total employee wages

---
+++ src/Company_Employee_Wage.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Author -> Arafath Baig
+ * PHP Version -> 8.0.9
+ * Class to Compute Employee Wage Problem
+ */
+class CompanyEmpWage
+{
+    /**
+     * Function for multiple companies and 
+     * storing name and total wage into arrays
+     * Printing the array
+     */
+    public function numOfCompanies()
+    {
+        $name = array();
+        $totalWage = array();
+        $n = readline("Number of Companies: ");
+        for ($i = 0; $i < $n; $i++) {
+            $name[$i] = readline('Enter Name of Company: ');
+            $wage = readline('Enter Employee Wage Per Hour: ');
+            $days = readline('Enter Maximum working days per month: ');
+            $hours = readline('Enter Maximum working hours per month: ');
+            echo "Employee Wage Computation For\n";
+            echo "***** " . $name[$i] . " *****\n";
+            $employeeWage = new Employee_Wage($wage, $days, $hours);
+            $totalWage[$i] = $employeeWage->monthlyWage();
+        }
+        for ($i = 0; $i < $n; $i++) {
+            echo "\nName of Company:: " . $name[$i];
+            echo "\nTotal Salary:: " . $totalWage[$i];
+        }
+    }
+}
