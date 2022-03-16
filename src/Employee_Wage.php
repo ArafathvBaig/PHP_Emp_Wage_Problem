f40c3c10132df6b91a30cfd6e8726edec247becf
“ArafathvBaig” <arafathbaig1997@gmail.com>
2022-03-16 16:11:14 +0530

    Updated: Employee wage with part time employee and wage

--- src/Employee_Wage.php
+++ src/Employee_Wage.php
@@ -1,5 +1,5 @@
 <?php
-echo "Welcome to Employee Wage Computation Problem";
+echo "Welcome to Employee Wage Computation Problem\n";
 
 /**
  * Author -> Arafath Baig
@@ -10,18 +10,24 @@ class Employee_Wage
 {
     public $WAGE_PER_HR = 20;
     public $FULL_TIME_WORKING_HRS = 8;
+    public $PART_TIME_WORKING_HRS = 4;
+    public $IS_FILL_TIME = 2;
+    public $IS_PART_TIME = 1;
+    public $IS_ABSENT = 0;
 
     /**
      * Function to Check Employee is Present or Absent
-     * Non-Parameterized function
      * Returns working hrs
-     * Using rand() to generate the random of 0 or 1
+     * Non-Parameterized Function
      */
     function attendance()
     {
-        $empCheck = rand(0, 1);
-        if ($empCheck == 1) {
-            echo "Employee is Present\n";
+        $empCheck = rand(0, 2);
+        if ($empCheck == $this->IS_PART_TIME) {
+            echo "Part Time Employee\n";
+            return $this->PART_TIME_WORKING_HRS;
+        } elseif ($empCheck == $this->IS_FILL_TIME) {
+            echo "Full Time Employee\n";
             return $this->FULL_TIME_WORKING_HRS;
         } else {
             echo "Employee is Absent\n";
@@ -31,7 +37,7 @@ class Employee_Wage
 
     /**
      * Function to Calculate Daily Wage
-     * Non-Parameterized function
+     * Non-Parameterized Function
      * Printing the daily wage to the output
      */
     function dailyWage()
